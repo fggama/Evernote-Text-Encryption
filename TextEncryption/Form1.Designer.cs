00904c8085ebe8a96a020e94b21305534ec49710
Freddy Garcia <garciafr@epsas.com>
2019-06-19 16:05:00 -0400

    Version inicial
    Encripcion de texto en Evernote, versión inicial

---
+++ TextEncryption/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace TextEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.butLeer = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.butActualizar = new System.Windows.Forms.Button();
+            this.cbMostrar = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNota = new System.Windows.Forms.TextBox();
+            this.tbContrasenia = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(287, 149);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // butLeer
+            // 
+            this.butLeer.BackColor = System.Drawing.Color.DarkKhaki;
+            this.butLeer.FlatAppearance.BorderSize = 0;
+            this.butLeer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Khaki;
+            this.butLeer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butLeer.Image = ((System.Drawing.Image)(resources.GetObject("butLeer.Image")));
+            this.butLeer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.butLeer.Location = new System.Drawing.Point(31, 66);
+            this.butLeer.Name = "butLeer";
+            this.butLeer.Size = new System.Drawing.Size(112, 23);
+            this.butLeer.TabIndex = 1;
+            this.butLeer.Text = "leer Nota";
+            this.butLeer.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolTip1.SetToolTip(this.butLeer, "Recupera la Nota desde Evernote");
+            this.butLeer.UseVisualStyleBackColor = false;
+            this.butLeer.Click += new System.EventHandler(this.butLeer_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(190, 176);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(287, 145);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // butActualizar
+            // 
+            this.butActualizar.BackColor = System.Drawing.Color.DarkKhaki;
+            this.butActualizar.FlatAppearance.BorderSize = 0;
+            this.butActualizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Khaki;
+            this.butActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butActualizar.Image = ((System.Drawing.Image)(resources.GetObject("butActualizar.Image")));
+            this.butActualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.butActualizar.Location = new System.Drawing.Point(31, 230);
+            this.butActualizar.Name = "butActualizar";
+            this.butActualizar.Size = new System.Drawing.Size(112, 29);
+            this.butActualizar.TabIndex = 4;
+            this.butActualizar.Text = "actualizar Nota";
+            this.butActualizar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolTip1.SetToolTip(this.butActualizar, "Actualiza la Nota con el Texto cifrado");
+            this.butActualizar.UseVisualStyleBackColor = false;
+            this.butActualizar.Click += new System.EventHandler(this.butActualizar_Click);
+            // 
+            // cbMostrar
+            // 
+            this.cbMostrar.AutoSize = true;
+            this.cbMostrar.Enabled = false;
+            this.cbMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbMostrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cbMostrar.Location = new System.Drawing.Point(80, 303);
+            this.cbMostrar.Name = "cbMostrar";
+            this.cbMostrar.Size = new System.Drawing.Size(99, 17);
+            this.cbMostrar.TabIndex = 6;
+            this.cbMostrar.Text = "Mostrar texto >>";
+            this.cbMostrar.UseVisualStyleBackColor = true;
+            this.cbMostrar.CheckedChanged += new System.EventHandler(this.cbMostrar_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Beige;
+            this.panel1.Controls.Add(this.tbContrasenia);
+            this.panel1.Controls.Add(this.cbMostrar);
+            this.panel1.Controls.Add(this.tbNota);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.butLeer);
+            this.panel1.Controls.Add(this.butActualizar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 334);
+            this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nota";
+            // 
+            // tbNota
+            // 
+            this.tbNota.Location = new System.Drawing.Point(80, 6);
+            this.tbNota.Name = "tbNota";
+            this.tbNota.Size = new System.Drawing.Size(81, 20);
+            this.tbNota.TabIndex = 10;
+            this.tbNota.Text = "Contraseñas";
+            this.toolTip1.SetToolTip(this.tbNota, "Nombre de la Nota en Evernote");
+            // 
+            // tbContrasenia
+            // 
+            this.tbContrasenia.Location = new System.Drawing.Point(80, 28);
+            this.tbContrasenia.Name = "tbContrasenia";
+            this.tbContrasenia.PasswordChar = '*';
+            this.tbContrasenia.Size = new System.Drawing.Size(81, 20);
+            this.tbContrasenia.TabIndex = 11;
+            this.tbContrasenia.Text = "Veloci R4pt0r";
+            this.toolTip1.SetToolTip(this.tbContrasenia, "Contraseña para el cifrado/descifrado de la Nota");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(487, 334);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cifrar Texto Nota Evernote";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button butLeer;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button butActualizar;
+        private System.Windows.Forms.CheckBox cbMostrar;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbContrasenia;
+        private System.Windows.Forms.TextBox tbNota;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TextEncryption/Form1.cs
@@ -0,0 +1,203 @@
+﻿using EvernoteSDK;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TextEncryption
+{
+    public partial class Form1 : Form
+    {
+        private ENNote _note;
+        private ENNotebook _noteBook;
+        private ENNoteRef _noteRef;
+        public Form1()
+        {
+            InitializeComponent();
+            _cifrado = "";
+            _plain = "";
+            
+            /* Modifique los valores de los 'Recursos' (propiedades del proyecto) para:
+             *** Nota
+             *** Contrasenia
+             *** Token
+             *** StoreUrl
+             */
+
+            tbNota.Text = Properties.Resources.ResourceManager.GetString("Nota");
+            tbContrasenia.Text = Properties.Resources.ResourceManager.GetString("Contrasenia");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ENSession.SetSharedSessionDeveloperToken(Properties.Resources.ResourceManager.GetString("Token"),
+                Properties.Resources.ResourceManager.GetString("StoreUrl"));
+        }
+
+        private void butLeer_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            try
+            {
+                if (ENSession.SharedSession.IsAuthenticated == false)
+                    ENSession.SharedSession.AuthenticateToEvernote();
+                if (!ENSession.SharedSession.IsAuthenticated)
+                    throw new Exception("No autenticado");
+                
+                string textToFind = "";
+                
+                List<ENNotebook> myNotebookList = ENSession.SharedSession.ListNotebooks();
+                cbMostrar.Enabled = false;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                _plain = "";
+                _cifrado = "";
+                string texto_nota = "";
+                
+                foreach (ENNotebook en in myNotebookList)
+                {
+                    List<ENSessionFindNotesResult> myResultsList = ENSession.SharedSession.FindNotes(ENNoteSearch.NoteSearch(textToFind), en,
+                    ENSession.SearchScope.All, ENSession.SortOrder.RecentlyUpdated, 500);
+
+                    foreach (ENSessionFindNotesResult note in myResultsList)
+                    {
+                        if (note.Title.Equals(tbNota.Text))
+                        {
+                            _noteBook = en;
+                            _noteRef = note.NoteRef;
+                            _note = ENSession.SharedSession.DownloadNote(note.NoteRef);
+                            texto_nota += _note.TextContent;
+                        }
+                    }
+                    Application.DoEvents();
+                }
+                if (texto_nota.Length > 0)
+                {
+                    _plain = StringCipher.Decrypt(texto_nota, tbContrasenia.Text);
+                    _cifrado = texto_nota;
+                    textBox1.Text = _cifrado;
+                    textBox2.Text = _plain;
+                    cbMostrar.Enabled = true;
+                    cbMostrar.Checked = true;
+                    textBox2.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Leer Nota", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            Cursor = Cursors.Default;
+        }
+       
+        private void butActualizar_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("???", "Actualizar Nota", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (MessageBox.Show("Se actualizará la Nota de Evernote, desea continuar?", "Actualizar Nota", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                return;
+
+            Cursor = Cursors.WaitCursor;
+            try
+            {
+                if (ENSession.SharedSession.IsAuthenticated == false)
+                    ENSession.SharedSession.AuthenticateToEvernote();
+                if (!ENSession.SharedSession.IsAuthenticated)
+                    throw new Exception("No autenticado");
+
+                string encryptedstring = StringCipher.Encrypt(textBox2.Text, tbContrasenia.Text);
+                if (_note != null)
+                {
+                    ENNote replace = new ENNote();
+                    replace.Content = ENNoteContent.NoteContentWithString(encryptedstring);
+                    replace.Title = _note.Title;
+                    if (_note.TagNames != null)
+                        replace.TagNames = _note.TagNames;
+
+                    ENSession.SharedSession.UploadNote(replace, ENSession.UploadPolicy.Replace, _noteBook, _noteRef);
+                }
+                textBox1.Text = encryptedstring;
+                MessageBox.Show("Nota actualizada!", "Actualizar Nota", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Actualizar Nota", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            Cursor = Cursors.Default;
+        }
+
+        private void cbMostrar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbMostrar.Checked)
+                textBox2.ForeColor = Color.Black;
+            else
+                textBox2.ForeColor = Color.White;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+
+        private string _plain;
+        private string _cifrado;
+        private void textBox2_Enter(object sender, EventArgs e) 
+        {
+            if (_plain.Length == 0)
+                _plain = textBox2.Text;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (_plain.Length > 0 && _plain != textBox2.Text)
+            {
+                try
+                {
+                    textBox1.Text = StringCipher.Encrypt(textBox2.Text, tbContrasenia.Text);
+                    _plain = textBox2.Text;
+                    cbMostrar.Enabled = true;
+                    cbMostrar.Checked = true;
+                    butActualizar.Focus();
+                }
+                catch (Exception ex)
+                {
+                    if (MessageBox.Show(ex.Message + "\r\nRestaurar valor anterior?", "Cifrar", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                        textBox2.Text = _plain;
+                    textBox2.Focus();
+                }
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (_cifrado.Length == 0)
+                _cifrado = textBox1.Text;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (_cifrado.Length > 0 && _cifrado != textBox1.Text)
+            {
+                try
+                {
+                    textBox2.Text = StringCipher.Decrypt(textBox1.Text, tbContrasenia.Text);
+                    _cifrado = textBox1.Text;
+                    cbMostrar.Enabled = true;
+                    cbMostrar.Checked = true;
+                    butActualizar.Focus();
+
+                }
+                catch (Exception ex)
+                {
+                    if (MessageBox.Show(ex.Message + "\r\nRestaurar valor anterior?", "Descifrar", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                        textBox1.Text = _cifrado;
+                    textBox1.Focus();
+                }
+            }
+        }
+    }
+}
